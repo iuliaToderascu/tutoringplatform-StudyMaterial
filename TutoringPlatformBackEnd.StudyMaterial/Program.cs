e6c982d6ade548854a84f3f2299e551d501250de
iuliaToderascu <475741@student.fontys.nl>
2024-05-07 22:50:32 +0200

    CORS set

--- TutoringPlatformBackEnd.StudyMaterial/Program.cs
+++ TutoringPlatformBackEnd.StudyMaterial/Program.cs
@@ -4,6 +4,17 @@ using TutoringPlatformBackEnd.StudyMaterial.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // Allow requests from frontend domain
+               .AllowAnyMethod() // Allow any HTTP method
+               .AllowAnyHeader(); // Allow any header
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -29,12 +40,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseCors("AllowFrontend"); // Apply CORS policy
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
